8bae0586aebfdd7bf7c4b15598ea0370457fc8e6
Burak Eren Karasu <burakeren.karasu@gmail.com>
2025-09-22 02:14:45 +0300

    layout tab changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,46 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+	DarkTheme,
+	DefaultTheme,
+	ThemeProvider,
+} from '@react-navigation/native'
+import { useFonts } from 'expo-font'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import 'react-native-reanimated'
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from '@/hooks/useColorScheme'
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+	const colorScheme = useColorScheme()
+	const [loaded] = useFonts({
+		SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+	})
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+	if (!loaded) {
+		// Async font loading only occurs in development.
+		return null
+	}
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider
+			value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+		>
+			<Stack
+				screenOptions={{
+					headerShown: false,
+				}}
+				initialRouteName="onboarding-1"
+			>
+				<Stack.Screen name="onboarding-1" />
+				<Stack.Screen name="onboarding-2" />
+				<Stack.Screen name="onboarding-3" />
+				<Stack.Screen name="login" />
+				<Stack.Screen name="signup" />
+				<Stack.Screen name="verify-otp" />
+				<Stack.Screen name="home" />
+				<Stack.Screen name="+not-found" />
+			</Stack>
+			<StatusBar style="auto" />
+		</ThemeProvider>
+	)
 }
