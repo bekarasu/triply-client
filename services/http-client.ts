fa748f10d6ee1cb759a57ef957d46e241aae0082
Burak Eren Karasu <burakeren.karasu@gmail.com>
2026-05-25 13:56:24 +0300

    Set default timeout to 60000ms if API_CONFIG.TIMEOUT is not defined

--- services/http-client.ts
+++ services/http-client.ts
@@ -29,7 +29,7 @@ class HttpClient {
 	private requestQueue: QueuedRequest[] = []
 
 	constructor() {
-		this.timeout = API_CONFIG.TIMEOUT
+		this.timeout = API_CONFIG.TIMEOUT || 60000
 	}
 
 	private normalizeHeaders(
